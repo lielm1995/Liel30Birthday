cee72ae07d3fb0e7daba00665a888f964e3b33b2
Liel Malihi <liel.malihi@Liels-MacBook-Pro.local>
2025-08-29 10:44:47 +0300

    fix: move towels down

--- src/constants/cards.ts
+++ src/constants/cards.ts
@@ -101,20 +101,6 @@ export const cards: ICard[] = [
     ],
   },
   {
-    id: 'towels',
-    image: towels,
-    socials: [
-      {
-        type: 'web',
-        link: 'https://rikmatex.com/',
-      },
-      {
-        type: 'whatsapp',
-        link: 'https://wa.me/972528859795',
-      },
-    ],
-  },
-  {
     id: 'photo-still',
     image: orDoga,
     socials: [
@@ -165,6 +151,20 @@ export const cards: ICard[] = [
       {
         type: 'whatsapp',
         link: 'https://wa.me/972509629030',
+      },
+    ],
+  },
+  {
+    id: 'towels',
+    image: towels,
+    socials: [
+      {
+        type: 'web',
+        link: 'https://rikmatex.com/',
+      },
+      {
+        type: 'whatsapp',
+        link: 'https://wa.me/972528859795',
       },
     ],
   },
